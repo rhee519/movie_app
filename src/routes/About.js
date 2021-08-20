5c54b6200a3cf21dfd434175af0bbde3d0cf45a2
rhee519 <rhee519@gmail.com>
2021-08-20 16:24:05 +0900

    210820 finished!

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About(props) {
+  console.log(props);
+  return (
+    <div className="about__container">
+      <span>This is About Page!</span>
+    </div>
+  );
+}
+
+export default About;
